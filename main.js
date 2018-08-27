2fc60019e17cee4c0ce5b29a3f350a0a7170eb12
mpsido <mpsido2@gmail.com>
2018-08-27 14:44:24 +0800

    Instaling eslint as a dev dependency

--- main.js
+++ main.js
@@ -2,17 +2,19 @@ var HID = require('node-hid');
 
 var devices = HID.devices();
 
+// eslint-disable-next-line no-console
 console.log(devices);
 
-var devices = HID.devices();
-var deviceInfo = devices.find( function(d) {
+var deviceInfo = HID.devices().find( function(d) {
     var isTeensy = d.manufacturer == "SatoshiLabs";
     return isTeensy;
 });
+
 if( deviceInfo ) {
-  var device = new HID.HID( deviceInfo.path );
-  // ... use device
+  // ... create device object like this: new HID.HID( deviceInfo.path );
+  // eslint-disable-next-line no-console
   console.log("Skycoin hardware is plugged in");
 } else {
+  // eslint-disable-next-line no-console
   console.log("404 Skycoin hardware NOT FOUND");
 }
