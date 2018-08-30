4e4dbb21a3065e04fd00a4eafdd2324f683dd8f4
mpsido <mpsido2@gmail.com>
2018-08-30 14:26:29 +0800

    Setting up google protobuf

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Javascript interface for the Skycoin hardware wallet
 
 [![Build Status](https://travis-ci.com/skycoin/hardware-wallet-js.svg?branch=master)](https://travis-ci.com/skycoin/hardware-wallet-js)
+
+Generate messages.js library:
+
+    ./node_modules/.bin/pbjs -t static-module -w commonjs -o protob/skycoin.js protob/messages.proto protob/types.proto
--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 var HID = require('node-hid');
+var protobuf = require("protobufjs");
+var messages = require('./protob/skycoin');
 
 var devices = HID.devices();
 
@@ -10,11 +12,27 @@ var deviceInfo = HID.devices().find( function(d) {
     return isTeensy;
 });
 
+
+function makeTrezorMessage(data, msgId) {
+    console.log(msgId, data.byteLength);
+
+}
+
 if( deviceInfo ) {
-  // ... create device object like this: new HID.HID( deviceInfo.path );
+  var device = new HID.HID( deviceInfo.path );
   // eslint-disable-next-line no-console
   console.log("Skycoin hardware is plugged in");
+  device.on("data", function(data) {
+    // eslint-disable-next-line no-console
+    console.log("Received data", data);
+    // device.write([0x00, 0x01, 0x01, 0x05, 0xff, 0xff]);
+  });
 } else {
   // eslint-disable-next-line no-console
   console.log("404 Skycoin hardware NOT FOUND");
+  var failMsg = new messages.Failure.create( { "message": "Ahyo"});
+  let buffer = messages.Failure.encode(failMsg).finish();
+  console.log(buffer);
+  console.log(`buffer = ${Array.prototype.toString.call(buffer)}`);
+  makeTrezorMessage(buffer, 1);
 }
