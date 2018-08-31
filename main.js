c3c7fd2169047cecc464561cca251f8bc8500139
mpsido <mpsido2@gmail.com>
2018-08-31 12:03:31 +0800

    Converting makeTrezorMessage function in javascript

--- main.js
+++ main.js
@@ -13,9 +13,32 @@ var deviceInfo = HID.devices().find( function(d) {
 });
 
 
-function makeTrezorMessage(data, msgId) {
-    console.log(msgId, data.byteLength);
+function makeTrezorMessage(buffer, msgId) {
+  u8Array = new Uint8Array(buffer);
+  var trezorMsg = new ArrayBuffer(10 + u8Array.byteLength - 1);
+  var dv = new DataView(trezorMsg);
+  dv.setUint8(0, 35); // #
+  dv.setUint8(1, 35); // #
+  dv.setUint16(2, msgId);
+  dv.setUint32(4, u8Array.byteLength);
+  dv.setUint8(8, 10); // '\n'
+  var trezorMsg8 = new Uint8Array(trezorMsg);
+  trezorMsg8.set(u8Array.slice(1), 9);
 
+  var lengthToWrite = u8Array.byteLength;
+  var chunks = [];
+  var j = 0;
+  while (lengthToWrite > 0) {
+    u64pack = new Uint8Array(64);
+    u64pack[0] = 63;
+    u64pack.set(trezorMsg8.slice(63*j, 63*(j+1)), 1);
+    lengthToWrite -= 63;
+    chunks[j] = u64pack;
+    j++;
+
+  }
+  console.log(chunks)
+  return chunks
 }
 
 if( deviceInfo ) {
@@ -32,7 +55,6 @@ if( deviceInfo ) {
   console.log("404 Skycoin hardware NOT FOUND");
   var failMsg = new messages.Failure.create( { "message": "Ahyo"});
   let buffer = messages.Failure.encode(failMsg).finish();
-  console.log(buffer);
   console.log(`buffer = ${Array.prototype.toString.call(buffer)}`);
-  makeTrezorMessage(buffer, 1);
+  makeTrezorMessage(buffer, 3);
 }
