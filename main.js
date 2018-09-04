267ec20fd94108f4ab5e744b4f8d3151baeeb2c9
mpsido <mpsido2@gmail.com>
2018-09-04 16:57:38 +0800

    Simplifying API test in main.js

--- main.js
+++ main.js
@@ -1,25 +1,10 @@
-const messages = require('./protob/skycoin');
 const deviceWallet = require('./device-wallet');
 
-if( deviceWallet.deviceAddressGen() === null ) {
+if( deviceWallet.getDevice() === null ) {
   // eslint-disable-next-line no-console
   console.log("404 Skycoin hardware NOT FOUND");
-  const failMsg = messages.Failure.create( {"message": "Ahyo"} );
-  const buffer = messages.Failure.encode(failMsg).finish();
-  const chunks = deviceWallet.makeTrezorMessage(buffer, 3);
-  if (chunks.length > 0) {
-    // eslint-disable-next-line no-console
-    console.log(chunks[0].toString());
-  }
 } else {
   // eslint-disable-next-line no-console
   console.log("Skycoin hardware is plugged in");
-  const message = "Failure";
-  const msgStructure = {
-      message
-  };
-  const msg = messages.Failure.create(msgStructure);
-  const buffer = messages.Failure.encode(msg).finish();
-  // eslint-disable-next-line no-console
-  console.log(buffer);
+  deviceWallet.deviceAddressGen(0, 0);
 }
