88fc243d49b60778739bf22e3d032131dd2a5992
Olemis Lang <olemis@simelo.tech>
2019-03-11 14:56:35 -0400

    [ci] refs #28 - Install libegl1-mesa-dev libgles2-mesa-dev
    ... according to mfailure essage in https://travis-ci.com/skycoin/hardware-wallet-js/jobs/183928830#L637-L647

--- ci-scripts/install-linux.sh
+++ ci-scripts/install-linux.sh
@@ -4,5 +4,5 @@ set -x
 
 sudo apt-get update;
 sudo apt install libudev-dev libusb-1.0-0-dev
-sudo apt install libsdl2-dev libsdl2-image-dev;
+sudo apt install libsdl2-dev libsdl2-image-dev libegl1-mesa-dev libgles2-mesa-dev;
 
