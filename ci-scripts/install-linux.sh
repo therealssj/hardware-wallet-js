d0497559b04a99242cdf8e837cc5acddb089d159
Olemis Lang <olemis@simelo.tech>
2019-03-11 14:07:17 -0400

    [ci] refs #38 - Travis install scripts for linux and osx. Changes in hardware-wallet setup

---
+++ ci-scripts/install-linux.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -x
+
+sudo apt-get update;
+sudo apt install libudev-dev libusb-1.0-0-dev
+sudo apt install libsdl2-dev libsdl2-image-dev;
+
---
+++ ci-scripts/install-osx.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -x
+
+brew update
+brew install libusb
+brew install sdl2_image sdl2 mesa mesalib-glw
+
