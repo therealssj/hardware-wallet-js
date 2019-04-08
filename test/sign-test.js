44ea2e142e0da11d7d05266299114e17be60a74a
Carlos Gutierrez Ramirez <dev0025@simelo.tech>
2019-04-08 04:39:23 +0000

    [test] refs #74 - Remove reference to testNotImplementedInFirmware in sign test

--- test/sign-test.js
+++ test/sign-test.js
@@ -2,7 +2,6 @@ const deviceWallet = require('../device-wallet');
 const utils = require('../utils');
 const rejectPromise = utils.rejectPromise;
 const timeout = utils.timeout;
-const testNotImplementedInFirmware = utils.testNotImplementedInFirmware;
 
 const setup = function () {
   return new Promise((resolve, reject) => {
